6832bc7ec3f8df6096b270ca20110aa2f74664cf
Quentin PANISSIER <contact@meshup.net>
2014-10-28 07:44:07 +0100

    Correction d'un problème d'accent au niveau des mois

--- routes/index.js
+++ routes/index.js
@@ -127,17 +127,17 @@ var genCSVFile = function( path, num, req, res, next ) {
 
         switch( month ) {
             case 'Janvier'  : month = '01'; break;
-            case 'Fevrier'  : month = '02'; break;
+            case 'Février'  : month = '02'; break;
             case 'Mars'     : month = '03'; break;
             case 'Avril'    : month = '04'; break;
             case 'Mai'      : month = '05'; break;
             case 'Juin'     : month = '06'; break;
             case 'Juillet'  : month = '07'; break;
-            case 'Aout'     : month = '08'; break;
+            case 'Août'     : month = '08'; break;
             case 'Septembre': month = '09'; break;
             case 'Octobre'  : month = '10'; break;
             case 'Novembre' : month = '11'; break;
-            case 'Decembre' : month = '12'; break;
+            case 'Décembre' : month = '12'; break;
             default:
                 errorHandler('Valeur inconnue pour le mois en cours.', path, res);
                 return;
